b7dc18565b81a97a529c5c7c2f7d691b853746ca
Nikolaj Baer <nikolaj.baer@gmail.com>
2019-10-04 23:24:04 -0700

    updated controls physics to match vehicle demo

--- VehicleBody.gd
+++ VehicleBody.gd
@@ -1,18 +1,31 @@
 extends VehicleBody
-	
+
+# Member variables
+const STEER_SPEED = 1
+const STEER_LIMIT = 0.4
+
+var steer_angle = 0
+var steer_target = 0
+
 onready var idle_sound = $EngineIdle
 onready var throttle_sound = $EngineThrottle
 onready var lspray = $LSpray
 onready var rspray = $RSpray
 
+export var engine_force_value = 400
 
-func _ready():
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Input.is_action_pressed('engine'):
-		self.engine_force = 8000
+func _physics_process(delta):
+	var fwd_mps = transform.basis.xform_inv(linear_velocity).x
+	
+	if Input.is_action_pressed("left"):
+		steer_target = STEER_LIMIT
+	elif Input.is_action_pressed("right"):
+		steer_target = -STEER_LIMIT
+	else:
+		steer_target = 0
+	
+	if Input.is_action_pressed("engine"):
+		engine_force = engine_force_value
 		lspray.emitting = true
 		rspray.emitting = true
 		idle_sound.stop()
@@ -21,29 +34,29 @@ func _process(delta):
 	else:
 		lspray.emitting = false
 		rspray.emitting = false
-		self.engine_force = 0
-		throttle_sound.stop()
-		if not idle_sound.playing:
-			idle_sound.play()
+		engine_force = 0
 	
-	if Input.is_action_pressed('brake'):
-		self.brake = 1000
-		self.engine_force = -1000
-	
-	if Input.is_action_pressed("left"):
-		if self.steering < .5:
-			self.steering += 0.01
-	elif Input.is_action_pressed("right"):
-		if self.steering > -.5:
-			self.steering -= 0.01
+	if Input.is_action_pressed("brake"):
+		if (fwd_mps >= -1):
+			engine_force = -engine_force_value
+		else:
+			brake = 1
 	else:
-		if self.steering < -0.01:
-			self.set_steering( self.steering + 0.01 )
-		elif self.steering > 0.01:
-			self.set_steering( self.steering - 0.01 )
+		brake = 0.0
+	
+	if steer_target < steer_angle:
+		steer_angle -= STEER_SPEED * delta
+		if steer_target > steer_angle:
+			steer_angle = steer_target
+	elif steer_target > steer_angle:
+		steer_angle += STEER_SPEED * delta
+		if steer_target < steer_angle:
+			steer_angle = steer_target
+	
+	steering = steer_angle
+
 
 func _on_VehicleBody_body_entered(body):
 	if body.get_collision_layer_bit(1):
 		print("Bang")
 		$CrashSound.play()
-
