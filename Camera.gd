2fa3b4078602c812081a892d7665503ade642f1e
Kevin Mooney <kmooney@gmail.com>
2019-10-06 01:43:52 +0000

    camera flies in

--- Camera.gd
+++ Camera.gd
@@ -9,9 +9,10 @@ export var autoturn_ray_aperture = 25
 export var autoturn_speed = 50
 var max_height = 1.0
 var min_height = 0.5
-
+var flying_in = true
 
 func _physics_process(dt):
+	
 	var vehicle_body = get_parent().get_parent().get_parent()
 	var target = vehicle_body.get_global_transform().origin
 	var pos = get_transform().origin
@@ -24,20 +25,35 @@ func _physics_process(dt):
 	# Regular delta follow
 	
 	# Check ranges
-	if (delta.length() < min_distance):
-		delta = delta.normalized()*min_distance
-	elif (delta.length() > max_distance):
-		delta = delta.normalized()*max_distance
-	
-	# Check upper and lower height
-	if ( delta.y > max_height):
-		delta.y = max_height
-	if ( delta.y < min_height):
-		delta.y = min_height
-	
+	if !flying_in:
+		if (delta.length() < min_distance):
+			delta = delta.normalized()*min_distance
+		elif (delta.length() > max_distance):
+			delta = delta.normalized()*max_distance
+		
+		# Check upper and lower height
+		if ( delta.y > max_height):
+			delta.y = max_height
+		if ( delta.y < min_height):
+			delta.y = min_height
+					
 	pos = target + delta
 	
 	look_at_from_position(pos, target, up)
+	if flying_in:
+		#translate(Vector3(1.0,0,0))
+		var d = target - get_transform().origin
+		print(d.length(), d/100.0)
+		translate(d/100.0)
+		if d.length() < max_distance:
+			flying_in = false
+			
+			
+		
+		
+		
+		
+	
 	
 	# Turn a little up or down
 	var t = get_transform()
